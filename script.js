14b3a3816a5abd691e284e4a0924122926358c0a
MaySamah <SamahAMMustafa@gmail.com>
2024-06-06 19:54:23 +0100

    Practice

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = "May";
+let age = 27;
+const dateOfBirth = 1997;
+
+var futureJob = "Web Developer";
+
+const message = "Hi my name is " + name + ", I am " + age + " years old and i will soon become a " + futureJob;
+console.log(message);
+
+// const message = `Hi my name is ${name} , I am ${age} years old and i will soon become a ${futureJob}`;
+// console.log(message);
